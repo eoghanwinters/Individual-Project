19bda22f72bf6e09fcedd7c42f8db5b8b3eaf636
eoghanwinters <55387284+eoghanwinters@users.noreply.github.com>
2019-11-06 09:32:25 +0000

    Update systemD-run.sh

--- systemD-run.sh
+++ systemD-run.sh
@@ -13,9 +13,10 @@ sudo rm -rf ${install_dir}
 sudo mkdir ${install_dir}
 sudo cp -r ./* ${install_dir}
 sudo chown -R pythonadm:pythonadm ${install_dir}
-sudo su - pythonadm
+sudo su - pythonadm << EOF
 cd ${install_dir}
 virtualenv -p python3 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+EOF
 sudo systemctl start flask-app
