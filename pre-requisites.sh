51b70c7867bd40b84c8c24a3cd608dbfc7c81c61
eoghanwinters <55387284+eoghanwinters@users.noreply.github.com>
2019-11-07 09:34:00 +0000

    Update pre-requisites.sh

--- pre-requisites.sh
+++ pre-requisites.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 sudo apt-get install -y virtualenv
 sudo apt-get install -y libmysqlclient-dev
-
+sudo apt-get install -y mysql-server
