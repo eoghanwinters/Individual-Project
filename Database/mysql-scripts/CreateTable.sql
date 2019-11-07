8be1373fbf76ba899b92c98dee906b14d5652feb
eoghanwinters <eoghanwinters1@hotmail.co.uk>
2019-11-07 16:21:19 +0000

    A working docker-compose deploy

--- Database/mysql-scripts/CreateTable.sql
+++ Database/mysql-scripts/CreateTable.sql
@@ -2,10 +2,9 @@
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
         id         INTEGER  NOT NULL AUTO_INCREMENT,
         first_name VARCHAR (20) NOT NULL,
         last_name  VARCHAR (20) NOT NULL,
@@ -21,10 +20,9 @@ CREATE TABLE users (
 -- Table structure for table `exercises`
 --
 
-DROP TABLE IF EXISTS `exercises`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE exercises
+CREATE TABLE IF NOT EXISTS exercises
              (
                           id            INTEGER NOT NULL AUTO_INCREMENT,
                           exercise_name VARCHAR(50) NOT NULL,
